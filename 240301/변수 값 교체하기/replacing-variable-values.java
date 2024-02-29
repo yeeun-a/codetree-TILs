c91826fcc358572e37e503758ca7e1bd0904de8d
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-01 01:16:40 +0900

    Title: 변수 값 교체하기, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

--- 240301/변수 값 교체하기/replacing-variable-values.java
+++ 240301/변수 값 교체하기/replacing-variable-values.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int a = 3;
-        int a = 6;
+        a = 6; // 동일한 변수에 새로운 값을 할당할 때는 'int' 키워드 없이 작성
         System.out.print(a);
     }
 }
