b440d3b785ab4b32b630fb64e56f9df32b89f03d
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-01 01:03:31 +0900

    Title: 달에서 무게 구하기, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240301/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 13;
+        float b = 0.165f;
+
+        System.out.printf("%s * %f = %f", a, b, a*b);
+    }
+}
