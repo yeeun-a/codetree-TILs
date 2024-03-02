009f70007dce3bf79076b23530917dbdd9f6f2e9
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-02 23:08:58 +0900

    Title: 점수 입출력, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240302/점수 입출력/enter-int-and-print-score.java
+++ 240302/점수 입출력/enter-int-and-print-score.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.printf("Your score is %d point.", n)
+        System.out.printf("Your score is %d point.", n);
     }
 }
