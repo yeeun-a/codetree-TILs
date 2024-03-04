fbe7d44b4f70df30220ccb9e30099967be24033b
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-04 20:22:12 +0900

    Title: 실수 입력받아 계산 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240304/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double a = sc.nextDouble();
+        Double result = a + 1.5;
+        System.out.printf("%.2f", result)
+    }
+}
