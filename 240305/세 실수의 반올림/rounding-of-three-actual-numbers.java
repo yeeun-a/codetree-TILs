57f0f4854a76d7121d5d84ac88531dd0ab6375ad
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-05 13:56:30 +0900

    Title: 세 실수의 반올림, Time: 179ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240305/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
+        Double c = sc.nextDouble();
+
+        System.out.printf("%.3f\n", a);
+        System.out.printf("%.3f\n", b);
+        System.out.printf("%.3f\n", c);
+    }
+}
