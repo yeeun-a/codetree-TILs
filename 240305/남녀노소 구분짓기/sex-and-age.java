dd69488252b0324ea7fa2b36354ddc56f0fd9672
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-05 14:15:09 +0900

    Title: 남녀노소 구분짓기, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int age = sc.nextInt();
+
+        if (s == 0) {
+            if (age >= 19) {System.out.println("MAN");}
+            else {System.out.println("BOY");}
+        } else {
+            if (age >= 19) {System.out.println("WOMAN");}
+            else {System.out.println("GIRL");}
+        }
+    }
+}
