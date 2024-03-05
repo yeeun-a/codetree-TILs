d0cae3a5af532c770bf3719632b1cfbc12c453b2
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-05 16:16:50 +0900

    Title: 1시간 뒤 시간 출력, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(":");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        a += 1;
+
+        System.out.print(a + ":" + b);
+    }
+}
