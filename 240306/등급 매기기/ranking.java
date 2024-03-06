e6bf1a1b4a6870e3f2bef9157ca60b24358df42c
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-06 10:53:45 +0900

    Title: 등급 매기기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/등급 매기기/ranking.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num >= 90) { System.out.print("A"); }
+        else if (num >= 80) { System.out.print("B"); }
+        else if (num >= 70) { System.out.print("C"); }
+        else if (num >= 60) { System.out.print("D"); }
+        else { System.out.print("F"); } 
+    }
+}
