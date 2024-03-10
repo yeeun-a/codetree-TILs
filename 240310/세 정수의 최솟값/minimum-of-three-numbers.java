ce6ed5b71e1a9ddf3fc7a563f81da6284bac37c4
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-10 16:53:55 +0900

    Title: 세 정수의 최솟값, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240310/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // a가 최솟값인 경우
+        if (a <= b && a <= c) {
+            System.out.print(a);
+        } else if (b <= a && b <= c) {
+            System.out.print(b);
+        } else {
+            System.out.print(c);
+        }
+    }
+}
