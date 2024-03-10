937b59f5fb278394bec60c2686d107bf7d993cda
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-10 23:26:18 +0900

    Title: 코로나 메뉴얼, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240310/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char a_sym = sc.next().charAt(0);
+        int a_temp = sc.nextInt();
+        char b_sym = sc.next().charAt(0);
+        int b_temp = sc.nextInt();
+        char c_sym = sc.next().charAt(0);
+        int c_temp = sc.nextInt();
+
+        if (a_sym == 'Y' && a_temp >= 37) {
+            if ((b_sym == 'Y' && b_temp >= 37)|| (c_sym == 'Y' && c_temp >= 37))
+                System.out.print('E');
+        } else {
+            if ((b_sym == 'Y' && b_temp >= 37) && (c_sym == 'Y' && c_temp >= 37))
+                System.out.print('N');
+        }
+    }
+}
