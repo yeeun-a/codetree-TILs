bcb54c806bfda4269142e6d8c2c638169612d27b
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-10 23:05:00 +0900

    Title: 일 수 구하기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240310/일 수 구하기/number-of-days-in-month.java
+++ 240310/일 수 구하기/number-of-days-in-month.java
@@ -5,12 +5,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
 
-        if (m % 2 == 0) {
-            if (m == 2)
-                System.out.print(28);
+        if (m == 2)
+            System.out.print(28);
+        else if (m <= 7) {
+            if (m % 2 == 0)
+                System.out.print(30);
+            else
+                System.out.print(31);
+        } else {
+            if (m % 2 == 0)
+                System.out.print(31);
             else
                 System.out.print(30);
-        } else
-            System.out.print(31);
+        }
     }
 }
