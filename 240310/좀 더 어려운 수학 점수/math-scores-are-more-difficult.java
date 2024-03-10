06b00dcde85651b90e38c58e1f35eff860ecbf9f
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-10 22:27:09 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240310/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
+++ 240310/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -8,11 +8,16 @@ public class Main {
         int bMath = sc.nextInt();
         int bEng = sc.nextInt();
 
-        if (aMath == bMath)
-            System.out.print(aEng > bEng ? "A" : "B");
-        else if (aMath > bMath)
-            System.out.print("A");
+        // if (aMath == bMath)
+        //     System.out.print(aEng > bEng ? "A" : "B");
+        // else if (aMath > bMath)
+        //     System.out.print("A");
+        // else
+        //     System.out.print("B");
+
+        if(a_math > b_math || (a_math == b_math && a_eng > b_eng))
+            System.out.println("A");
         else
-            System.out.print("B");
+            System.out.println("B");
     }
 }
