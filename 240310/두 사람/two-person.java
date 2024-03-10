ee0f7fb42a4a32adc96a9f202160520942f8aa25
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-03-10 22:36:09 +0900

    Title: 두 사람, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240310/두 사람/two-person.java
+++ 240310/두 사람/two-person.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int a_age = sc.nextInt();
-        char a_sex = sc.next().charAt(0);
+        char a_gen = sc.next().charAt(0);
         int b_age = sc.nextInt();
-        char b_sex = sc.next().charAt(0);
+        char b_gen = sc.next().charAt(0);
 
-        if ((a_age >= 19 && a_sex == 'M') || (b_age >= 19 && b_sex == 'M'))
+        if ((a_age >= 19 && a_gen == 'M') || (b_age >= 19 && b_gen == 'M'))
             System.out.print(1);
         else
             System.out.print(0);
