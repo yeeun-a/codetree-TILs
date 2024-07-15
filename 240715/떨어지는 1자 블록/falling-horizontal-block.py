68743128bbd14d1249d3e3544c85fe32913b52cb
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-07-15 21:59:03 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240715/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,23 @@
+import sys
+r=sys.stdin.readline
+
+n,m,k=map(int,r().split())
+graph=[]
+for _ in range(n):
+    graph.append(list(map(int,r().split())))
+
+k-=1
+y=0
+while 1:
+    flag = 1
+    y+=1
+    for x in range(m):
+        if graph[y][k+x] == 1:
+            flag = 0
+            break
+    if not flag:
+        break
+for x in range(m):
+    graph[y-1][k+x] = 1
+for e in graph:
+    print(*e)
