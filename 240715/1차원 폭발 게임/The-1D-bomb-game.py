f2945156a91ffc2885b8e7c0af385fc511f9d1cd
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-07-15 22:45:53 +0900

    Title: 1차원 폭발 게임, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,35 @@
+import sys
+r=sys.stdin.readline
+
+n,m=map(int,r().split())
+a=[]
+for _ in range(n):
+    a.append(int(r()))
+
+end = n-1
+while 1:
+    last_num = 0
+    last_idx = -1
+    cnt = 0
+    flag = 1
+    pre = end
+    for i in range(len(a)):
+        e = a[i]
+        if flag:
+            a[last_idx]=e
+            if i == end:
+                end = last_idx
+            last_idx+=1
+            continue
+        if e == last_num:
+            cnt += 1
+        else:
+            last_num = e
+            cnt = 1
+            last_idx = i
+        if cnt >= m:
+            flag=1
+    if pre == end:
+        break
+print(end+1)
+print(*a[:end+1])
