44d7e49e080a0fe5f3b63be350d91d047cb241f9
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-07-15 19:56:23 +0900

    Title: 금 채굴하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240715/금 채굴하기/gold-mining.py
@@ -0,0 +1,25 @@
+import sys
+r=sys.stdin.readline
+
+n,m=map(int,r().split())
+coin=[]
+for y in range(n):
+    temp = list(map(int,r().split()))
+    for x in range(m):
+        if temp[x] == 1:
+            coin.append((y,x))
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(n+1):
+            cost = 0
+            cost -= k*k + (k+1)*(k+1)
+            res = 0
+            for y,x in coin:
+                if abs(y-i) + abs(x-j) <= k:
+                    cost += m
+                    res += 1
+            if cost >= 0:
+                ans = max(ans, res)
+print(ans)
