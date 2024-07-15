fb77ea7973b4008eb5baeff0d8bd748e6b2756ce
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-07-15 19:39:10 +0900

    Title: 트로미노, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/트로미노/tromino.py
@@ -0,0 +1,31 @@
+import sys
+r=sys.stdin.readline
+
+n,m=map(int,r().split())
+
+shape = [
+    [(0,0), (1,0), (1,1)],
+    [(0,1), (1,0), (1,1)],
+    [(0,0), (0,1), (1,1)],
+    [(0,0), (1,0), (0,1)],
+    [(0,0), (1,0), (2,0)],
+    [(0,0), (0,1), (0,2)],
+]
+
+graph=[]
+for _ in range(n):
+    graph.append(list(map(int,r().split())))
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        for cord in shape:
+            flag = 1
+            res = 0
+            for y, x in cord:
+                if i+y >= n or j+x >= m:
+                    flag = 0
+                    break
+                res += graph[i+y][j+x]
+            ans = max(ans, res)
+print(ans)
