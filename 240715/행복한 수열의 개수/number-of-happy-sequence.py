d32e16f24ada4acb130adfcd01b1af65290bcd01
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-07-15 19:29:24 +0900

    Title: 행복한 수열의 개수, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240715/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240715/행복한 수열의 개수/number-of-happy-sequence.py
@@ -8,19 +8,28 @@ for _ in range(n):
 
 ans=0
 for i in range(n):
-    store=[0]*101
-    for j in range(n):
-        store[graph[i][j]]+=1
-        if store[graph[i][j]] >= m:
-            ans+=1
-            break          
+    last_num=graph[i][0]
+    res=1
+    for j in range(1,n):
+        if last_num==graph[i][j]:
+            res+=1
+            if res >= m:
+                ans+=1
+                break
+        else:
+            res=1
+            last_num=graph[i][j]
 
-
-for i in range(n)::
-    store=[0]*101
-    for j in range(n):
-        store[graph[j][i]]+=1
-        if store[graph[j][i]] >= m:
-            ans+=1
-            break
+for i in range(n):
+    last_num=graph[0][i]
+    res=1
+    for j in range(1,n):
+        if last_num==graph[j][i]:
+            res+=1
+            if res >= m:
+                ans+=1
+                break
+        else:
+            res=1
+            last_num=graph[j][i]
 print(ans)
