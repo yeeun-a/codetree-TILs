3f1261398e9ee99e500256f21ecd583b684fc786
YEEUN CHOI <123927777+yeeun-a@users.noreply.github.com>
2024-07-29 22:28:25 +0900

    Title: 신기한 타일 뒤집기, Time: 146ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240729/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240729/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,37 +1,44 @@
-def flip_tiles(commands):
+def flip_tiles(n, commands):
     tiles = {}
     position = 0
-
+    
     for command in commands:
         x, direction = command.split()
         x = int(x)
         
-        if direction == 'L':
+        if direction == "L":
             for i in range(position, position - x, -1):
                 if i in tiles:
-                    tiles[i] = 'W' if tiles[i] == 'B' else 'B'
+                    if tiles[i] == "B":
+                        tiles[i] = "W"
+                    else:
+                        del tiles[i]
                 else:
-                    tiles[i] = 'W'
+                    tiles[i] = "W"
             position -= (x - 1)
-        elif direction == 'R':
+        
+        elif direction == "R":
             for i in range(position, position + x):
                 if i in tiles:
-                    tiles[i] = 'B' if tiles[i] == 'W' else 'W'
+                    if tiles[i] == "W":
+                        tiles[i] = "B"
+                    else:
+                        del tiles[i]
                 else:
-                    tiles[i] = 'B'
+                    tiles[i] = "B"
             position += (x - 1)
 
-    white_count = sum(1 for color in tiles.values() if color == 'W')
-    black_count = sum(1 for color in tiles.values() if color == 'B')
-
+    white_count = sum(1 for color in tiles.values() if color == "W")
+    black_count = sum(1 for color in tiles.values() if color == "B")
+    
     return white_count, black_count
 
-# Input reading
+# 입력 받기
 n = int(input().strip())
 commands = [input().strip() for _ in range(n)]
 
-# Execute the commands and get the result
-white_count, black_count = flip_tiles(commands)
+# 결과 계산
+white_count, black_count = flip_tiles(n, commands)
 
-# Output the result
+# 결과 출력
 print(white_count, black_count)
